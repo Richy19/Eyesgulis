04a638fac4af765f74c6940033941d5420d528b7
Valter Strods <valters.strods@gmail.com>
2014-05-24 23:27:51 +0300

    the "unsupported hash" message should be output only once at start

--- decoder.py
+++ decoder.py
@@ -14,6 +14,12 @@ def decode(msg, key, hashsys='sha1', verbose=False):
 	swap = True
 	decoded = ""
 
+	try:
+		hash = hashlib.new(str(hashsys))
+	except ValueError:
+		print("Error: hash {h} is not supported, defaulting to sha1".format(h=hashsys))
+		hashsys = 'sha1'
+
 	for n, x in enumerate(new_msg):
 		if verbose:
 			print("Decoding message part {x} [{n1}/{n2}]".format(x=x, n1=n+1, n2=len(new_msg)))
@@ -24,11 +30,7 @@ def decode(msg, key, hashsys='sha1', verbose=False):
 			full_hash = new_msg[n] + new_msg[n-1]
 			for a in ascii:
 				for i in ascii:
-					try:
-						hash = hashlib.new(str(hashsys))
-					except ValueError:
-						print("Error: hash {h} does not exist, using sha1".format(h=hashsys))
-						hash = hashlib.sha1()
+					hash = hashlib.new(str(hashsys))
 					hash.update(a.encode() + i.encode() + key.encode())
 					if hash.hexdigest() == full_hash:
 						decoded += a + i
--- encoder.py
+++ encoder.py
@@ -3,17 +3,19 @@ import hashlib
 import sys
 
 def encode(msg, key, hashsys='sha1'):
+	try:
+		hash = hashlib.new(str(hashsys))
+	except ValueError:
+		print("Error: hash {h} is not supported, defaulting to sha1".format(h=hashsys))
+		hashsys='sha1'
+
 	msg = msg.replace('\r', "").replace('\n', '')
 	key = key.replace('\r', "").replace('\n', '')
 	encoded_msg = []
 	for x in ([msg[i:i+2] for i in range(0, len(msg), 2)]):
 		if len(x) == 1:
 			x = x + ' '
-		try:
-			hash = hashlib.new(str(hashsys))
-		except ValueError:
-			print("Error: hash {h} does not exist, using sha1".format(h=hashsys))
-			hash = hashlib.sha1()
+		hash = hashlib.new(str(hashsys))
 		hash.update(x.encode()+key.encode())
 		encoded_msg.append(hash.hexdigest()[int(len(hash.hexdigest())/2):])
 		encoded_msg.append(hash.hexdigest()[:int(len(hash.hexdigest())/2)])
