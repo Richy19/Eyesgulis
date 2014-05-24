7f4ab842f625fa888adf03d152be1b7fe878c78e
Eyes <eyesismine@gmail.com>
2014-05-24 12:46:31 +0100

    Fixed encoded messages to work properly. This does not use naut's shitty version

--- decoder.py
+++ decoder.py
@@ -13,37 +13,32 @@ def decode(msg, key):
 	swap = True
 	decoded = ""
 
-	for x in new_msg:
-		print("Decoding message part", x)
-		found = False
-		for i in ascii:
+	for n, x in enumerate(new_msg):
+		print("Decoding message part {x} [{n1}/{n2}]".format(x=x, n1=n+1, n2=len(new_msg)))
+		part_decoded = False
+		if n != 0 and n % 2 != 0:
+			full_hash = new_msg[n] + new_msg[n-1]
 			for a in ascii:
-				sha1 = hashlib.sha1()
-				sha1.update(i.encode() + a.encode() + key.encode())
-				if swap:
-					if sha1.hexdigest()[:20] == x:
-						print("Unswapped\t", sha1.hexdigest(), i, a)
-						decoded = decoded+i+a
-						swap = False
-						found = True
+				for i in ascii:
+					sha1 = hashlib.sha1()
+					sha1.update(a.encode() + i.encode() + key.encode())
+					if sha1.hexdigest() == full_hash:
+						decoded += a + i
+						part_decoded = True
 						break
-				else:
-					if sha1.hexdigest()[20:] == x:
-						print("Swapped\t", sha1.hexdigest(), i, a)
-						decoded = decoded+i+a
-						swap = True
-						found = True
-						break
-		if not found:
-			#print(i, a, decoded)
-			decoded = i + a + decoded
-	for i in ascii:
-		decoded = decoded.replace(i+i, i)
+					else:
+						continue
+		if part_decoded:
+			print("{fh} -> {a}{i}".format(fh=full_hash, a=a, i=i))
+		else:
+			print("No result found for hash, are you using the right key?")
+	#for i in ascii:
+		#decoded = decoded.replace(i+i, i)
+		
 	decoded = decoded.replace("/", "")
 	return decoded
 
 if __name__ == "__main__":
 	msg = input("Message to decode: ")
-	#msg = open('./f').read()
 	key = input("Key for message: ")
 	print("Decoded output:", decode(msg, key))		
--- encoder.py
+++ encoder.py
@@ -5,10 +5,18 @@ import sys
 input_str = input("Message to encode: ")
 key_str = input("Key for message: ")
 
+input_str.replace('\r', ' ')
+input_str.replace('\n', ' ')
+key_str = key_str.replace('\r',  ' ')
+key_str = key_str.replace('\n', ' ')
+#print(input_str)
 new_msg = [input_str[i:i+2] for i in range(0, len(input_str), 2)]
+#print(new_msg)
 encoded_msg = []
 
 for x in new_msg:
+	if len(x) == 1:
+		x = x + ' '
 	sha1 = hashlib.sha1()
 	sha1.update(x.encode() + key_str.encode())
 	encoded_msg.append(sha1.hexdigest()[20:])
