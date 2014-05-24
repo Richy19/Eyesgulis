c57b90fc8ab80a770c9e90fd33d2734f43d02093
Valter Strods <valters.strods@gmail.com>
2014-05-24 17:57:47 +0300

    cleanup for hash variables

--- decoder.py
+++ decoder.py
@@ -3,7 +3,7 @@
 import sys
 import hashlib
 
-def decode(msg, key, hashsys):
+def decode(msg, key, hashsys='sha1'):
 	ascii = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM',.?!@/ "
 	msg = msg.replace('\r', ' ')
 	msg = msg.replace('\n', ' ')
@@ -22,7 +22,11 @@ def decode(msg, key, hashsys):
 			full_hash = new_msg[n] + new_msg[n-1]
 			for a in ascii:
 				for i in ascii:
-					hash = hashlib.new(hashsys)
+					try:
+						hash = hashlib.new(str(hashsys))
+					except ValueError:
+						print("Error: hash {h} does not exist, using sha1".format(h=hashsys))
+						hash = hashlib.sha1()
 					hash.update(a.encode() + i.encode() + key.encode())
 					if hash.hexdigest() == full_hash:
 						decoded += a + i
